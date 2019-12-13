a5f912f7156b025b5bad75723af512531e4e4b25
carlos <carlosguillermoru@outlook.com>
2019-12-13 21:32:12 +0100

    Crea Getters, Setters y toString

--- src/main/java/com/carlos/dawcoffee/Bebida.java
+++ src/main/java/com/carlos/dawcoffee/Bebida.java
@@ -14,9 +14,42 @@ public class Bebida {
     private String codigo;
     private String nombre;
     
+    // Creamos constructor parametrizado
     public Bebida(double precio, String codigo, String nombre){
         this.precio =  precio;
         this.nombre = nombre;
         this.codigo = codigo;
     }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "La bebida " + this.nombre + " tiene el código " + this.codigo + " y cuesta " + this.precio;
+    }
+    
+
+    
 }
