261fdd660c16fbf39305392f9af00c366ebaa9be
carlos <carlosguillermoru@outlook.com>
2019-12-13 21:04:33 +0100

    Creamos los atributos de la clase cafetera, y el constructor por defecto

--- src/main/java/com/carlos/dawcoffee/Cafetera.java
+++ src/main/java/com/carlos/dawcoffee/Cafetera.java
@@ -10,5 +10,14 @@ package com.carlos.dawcoffee;
  * @author carlos
  */
 public class Cafetera {
+    private double saldoAcumulado = 0;
+    private double saldoCliente;
+    private double ventasRealizadas = 0;
+    private double nVentasRealizadas;
     
+    // Creamos un constructor por defecto. No necesitaremos un constructor parametrizado
+    
+    public Cafetera(){
+        
+    }
 }
