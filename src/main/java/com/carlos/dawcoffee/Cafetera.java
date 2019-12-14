2d473c4425806013128c599c054febc5ceb38f4d
carlos <carlosguillermoru@outlook.com>
2019-12-14 11:10:12 +0100

    Implementa selector de bebidas

--- src/main/java/com/carlos/dawcoffee/Cafetera.java
+++ src/main/java/com/carlos/dawcoffee/Cafetera.java
@@ -15,9 +15,43 @@ public class Cafetera {
     private double ventasRealizadas = 0;
     private double nVentasRealizadas;
     
+    
+
+    
     // Creamos un constructor por defecto. No necesitaremos un constructor parametrizado
     
     public Cafetera(){
         
     }
+    public String seleccionarBebida(String codigo){
+        double price = 0;
+        String name = null;
+        String code = null;
+        switch(codigo){
+            
+            case "00001A":
+                price = 0.5;
+                name = "Leche sin azúcar";
+                code = codigo;
+                break;
+            case "000010A":
+                price = 0.5;
+                name = "Leche con poco azúcar";
+                code = codigo;
+                break;
+            case "000011A":
+                price = 0.5;
+                name = "Leche con poco azúcar";
+                code = codigo;
+                break;
+            case "AAAAAAA":
+                Interfaz.entrarComoAdministrador();
+                break;
+            default:
+                return "La bebida seleccionada no existe, por favor, introduce una válida";
+        }
+        Bebida bebida = new Bebida(price, code, name);
+        return bebida.toString();
+    }
+
 }
