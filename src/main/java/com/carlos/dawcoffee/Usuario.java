79783f871e5184b1ae95d7909f2324933b7919fb
carlos <carlosguillermoru@outlook.com>
2019-12-14 10:50:45 +0100

    Añade atributos, constructor por defecto, métodos getters y setters y método socket, el cual usaremos para comprobar si el usuario y la contraseña que introduzcamos son o no correctos

--- src/main/java/com/carlos/dawcoffee/Usuario.java
+++ src/main/java/com/carlos/dawcoffee/Usuario.java
@@ -10,5 +10,29 @@ package com.carlos.dawcoffee;
  * @author carlos
  */
 public class Usuario {
-    
+
+    private String usuario = "root";
+    private String contrasenia = "root";
+
+    public Usuario(){
+
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    private String getContrasenia() {
+        return contrasenia;
+    }
+
+    private void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    public void socket(String user, String passwd){
+        if((user.equals(this.usuario))||(passwd==this.contrasenia)){
+            System.out.println("Bienvenido, " + this.usuario);
+        }
+    }
+
 }
