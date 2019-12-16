d0f1c55399cc23d3a2b0734fc00ffecc3a631fd1
carlos <carlosguillermoru@outlook.com>
2019-12-16 16:50:56 +0100

    Implementa constructor y getters / setters

--- src/main/java/com/carlos/dawcoffee/Deposito.java
+++ src/main/java/com/carlos/dawcoffee/Deposito.java
@@ -10,5 +10,78 @@ package com.carlos.dawcoffee;
  * @author carlos
  */
 public class Deposito {
+    private double cantidadMaxima;
+    private double cantidadReserva;
+    private double cantidadTotal;
+    private String nombre;
+    private boolean reserva;
+    
+    public Deposito(String nombre, double cantidadMaxima, double cantidadReserva){
+        //Como el depósito acaba de añadirse, por defecto indicamos que está vacío
+        this.cantidadTotal = 0;
+        this.reserva = true;
+        this.cantidadReserva = cantidadReserva;
+        this.cantidadMaxima = cantidadMaxima;
+        this.nombre = nombre;
+    }
+    public Deposito(String nombre, double cantidadMaxima, double cantidadReserva, double cantidadActual){
+        this.cantidadTotal = cantidadActual;
+        if(cantidadActual >= cantidadReserva){
+            this.reserva = false;
+        }else{
+            this.reserva = true;
+        }
+        this.reserva = true;
+        this.cantidadReserva = cantidadReserva;
+        this.cantidadMaxima = cantidadMaxima;
+        this.nombre = nombre;
+    }
+
+    public void rellenarCafetera(double cantidadAniadida){
+        this.cantidadTotal += cantidadAniadida;
+        if(this.cantidadTotal >= cantidadReserva){
+            this.reserva = false;
+        }
+    }
+    
+    public double getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
+    public void setCantidadMaxima(double cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+    public double getCantidadReserva() {
+        return cantidadReserva;
+    }
+
+    public void setCantidadReserva(double cantidadReserva) {
+        this.cantidadReserva = cantidadReserva;
+    }
+
+    public double getCantidadTotal() {
+        return cantidadTotal;
+    }
+
+    public void setCantidadTotal(double cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isReserva() {
+        return reserva;
+    }
+
+    public void setReserva(boolean reserva) {
+        this.reserva = reserva;
+    }
     
 }
